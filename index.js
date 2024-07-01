f10c75dc3cac4a5940559498fcc3294b02a212f9
TheHero29 <loukik.23bcs10059@ms.sst.scaler.com>
2024-07-01 11:23:53 +0530

    made a node server

--- index.js
+++ index.js
@@ -1,22 +1,71 @@
- import {promises as fs} from 'fs';
+const fs = require('fs');
+const path = require('path');
+// import 'index.html' as html;
+// const data = fs.readFileSync('file.txt', 'utf8');
 
-async function readFile() {
-    try {
-       const data = await fs.readFile('file.txt', 'utf8');
-       return data
-    } catch (err) {
-        console.error(err);
-    }
-}
-async function writeFile(data,newdata) {
-    try {
-        console.log('File content:', data.concat(newdata));
-        await fs.writeFile('file.txt',data.concat(newdata))
-    } catch (err) {
-        console.error(err);
-    }
-}
-readFile().then((data)=>{
-    writeFile(data,"!")
+// console.log('File before:', data);
+
+// fs.appendFileSync('file.txt', '!');
+
+// console.log('File after :', fs.readFileSync('file.txt', 'utf8'));
+
+// fs.mkdir('myDir',(err)=>
+//     {
+//         if(err) throw err;
+//         console.log('Directory created')
+//     }
+// );
+
+// fs.rm('./myDir',{ recursive: true, force: true },(err)=>{
+//     if(err) throw err;
+//     console.log('Directory removed')
+// });
+
+// fs.rename('file.txt','file2.txt',(err)=>{
+//     if(err) throw err;
+//     console.log('File renamed')
+// });
+
+// const p = '/home/loukik/Projects/learning-node/file2.txt';
+
+// console.log(path.dirname(p),path.extname(p),path.basename(p));
+
+// const src = '/home/loukik/Projects/learning-node/file2.txt';
+// const dest = '/home/loukik/Projects/learning-node/myDir/file2.txt';
+
+// fs.copyFile(src,dest,(err)=>{
+//     if(err) throw err;
+//     // console.log('File copied')
+// });
+
+// fs.unlink(src,(err)=>{
+//     if(err) throw err;
+//     console.log('File moved')
+// })
+
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    res.setHeader('Content-Type','text/html');
+    res.write(`
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Node server</title>
+        </head>
+        <body>
+        <h1>Namaskar Dunia</h1>
+        </body>
+        </html>
+        `);
+    res.end();
+});
+
+const port = 2525;
+
+const host = 'localhost';
+
+server.listen(port,host,()=>{
+    console.log(`Server running at http://${host}:${port}`);
 })
-
